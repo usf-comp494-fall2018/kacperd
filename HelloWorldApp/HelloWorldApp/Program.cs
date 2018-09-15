3f6c4cea39cfce52bf9374a2575918bb4704df38
Kacper Dudzinski <kacperdudzinski@stfrancis.edu>
2018-09-14 19:55:34 -0500

    tesing out commits (again)

--- HelloWorldApp/HelloWorldApp/Program.cs
+++ HelloWorldApp/HelloWorldApp/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorldApp
     {
         static void Main(string[] args)
         {
-            //TEST
+            //TEST 2
             Console.WriteLine("Hello from cs lab!");
         }
     }
