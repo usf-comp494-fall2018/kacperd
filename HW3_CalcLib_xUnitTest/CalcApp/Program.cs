ad00552e1fe0bb47eb3bcb5d8ed8791fbb3137d2
Kacper Dudzinski <kacperdudzinski@stfrancis.edu>
2018-10-17 19:56:13 -0500

    midterm update

---
+++ HW3_CalcLib_xUnitTest/CalcApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using CalculatorLib;
+
+namespace CalcApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Calculator calc = new Calculator();
+            double[] numbers;
+
+            //Console.WriteLine($"{calc.Mean(numbers)}");
+
+            //Console.WriteLine($"{calc.Median(numbers)}");
+
+            //string userFolder = Environment.GetFolderPath();
+
+            string cur_dir = Environment.CurrentDirectory;
+
+            string inputTextPath = Path.Combine(cur_dir, "CalcInput.txt");
+            string outputTextPath = Path.Combine(cur_dir, "CalcOutput.txt");
+
+            StreamReader textReader = File.OpenText(inputTextPath);
+            string line;
+            int i = 0;
+            while ((line = textReader.ReadLine()) != null)
+            {
+                numbers[i] = Convert.ToDouble(line);
+            }
+
+        }
+    }
+}
--- HW3_CalcLib_xUnitTest/CalculatorLib/Calculator.cs
+++ HW3_CalcLib_xUnitTest/CalculatorLib/Calculator.cs
@@ -27,6 +27,39 @@ namespace CalculatorLib
             return a/b;
         }
 
+        public double Mean(double[] arr)
+        {
+            double sum = 0;
+            int size;
+
+            size = arr.Length;
+            foreach (double i in arr)
+            {
+                sum = sum + i;
+            }
+
+            return sum / size;
+        }
+
+        public double Median(double[] arr)
+        {
+            //??? dont fully understand this
+            Array.Sort(arr);
+            int size = arr.Length;
+
+            //if arr is even
+            if (size % 2 == 0)
+            {
+                return (arr[(size / 2) - 1] + arr[(size / 2)]) / 2;
+            }
+            //if arr is odd
+            else
+                return arr[(size - 1) / 2];
+
+
+
+        }
+
     }
 
 }
